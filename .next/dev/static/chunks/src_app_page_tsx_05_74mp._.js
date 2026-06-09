5a53d6751e33e3d76872c108b718c4d0b6da71d2
Guilherme Lorenzini <Guimsl2004@gmail.com>
2026-06-09 14:38:27 -0300

    correcao tailwind no projeto remocao do "tailwindcss/preflight" e ajuste gitignore

--- .next/dev/static/chunks/src_app_page_tsx_05_74mp._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0z~i_next_dist_02siueg._.js"],
-    source: "dynamic"
-});
